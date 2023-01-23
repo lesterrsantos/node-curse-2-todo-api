d2733582c19c433bc1db451e3d7a5b7dbe6538ce
Lester René Santos Martínez <lester.santos@datys.cu>
2023-01-23 13:50:16 -0500

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -59,7 +59,7 @@ UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = "auth";
   var token = jwt
-    .sign({ _id: user._id.toHexString(), access }, "abc123")
+    .sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET)
     .toString();
 
   // user.tokens.push({access, token});
@@ -86,7 +86,7 @@ UserSchema.statics.findByToken = function (token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, "abc123");
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (err) {
     // return new Promise((resolve, reject) => {
     //   reject();
